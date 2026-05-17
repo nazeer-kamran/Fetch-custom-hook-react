c238df8f5b187ed4ecb0f2f5fbd8d0cb70c7a1a8
nazeer-kamran <nazeerkamran111@gmail.com>
2026-05-17 07:24:38 -0400

    Added new a component

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import useFetch from './UseFetch'
+import './FetchData.css'
+
+const FetchYogaData = () => {
+    const [data] = useFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
+  return (
+    <>
+     <h1 className='usefetch_heading'>Use Fetch Custom Hook to Display Yoga Benefits</h1>
+     <ul className='list_data_main'>
+        {data && data.map((e, index) => (
+           <>
+            <li className='list_data' key={index} >
+                <h3>{e.name}</h3>
+                <p><strong>Benefits: </strong>{e.benefits}</p>
+                <p><strong>Time Duration: </strong>{e.time_duration}</p>
+            </li>
+           </> 
+        ))}
+     </ul>
+    </>
+  )
+}
+
+export default FetchYogaData
